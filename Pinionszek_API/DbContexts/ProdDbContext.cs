8d4c52d12dde3df5843d1706e8581126014a63e9
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-03-02 16:30:45 +0100

    Added db context

---
+++ Pinionszek_API/DbContexts/ProdDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pinionszek_API.DbContexts
+{
+    public class ProdDbContext : DbContext
+    {
+        public ProdDbContext()
+        {
+        }
+
+        public ProdDbContext(DbContextOptions opt) : base(opt)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
