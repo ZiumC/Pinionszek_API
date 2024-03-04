3a8a1a1e665f52cdaac289b03eb448afd7e11265
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-03-04 12:50:29 +0100

    Added references between:Payment-PaymentStatus, migration hasn't been modified yet

--- Pinionszek_API/Models/DatabaseModel/Payment.cs
+++ Pinionszek_API/Models/DatabaseModel/Payment.cs
@@ -1,4 +1,6 @@
-﻿namespace Pinionszek_API.Models.DatabaseModel
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pinionszek_API.Models.DatabaseModel
 {
     public class Payment
     {
@@ -8,5 +10,9 @@
         public decimal Refund { get; set; }
         public string Message { get; set; }
         public DateTime? PaymentDate { get; set; }
+
+        public int IdPaymentStatus { get; set; }
+        [ForeignKey(nameof(IdPaymentStatus))]
+        public virtual PaymentStatus PaymentStatus { get; set; }
     }
 }
--- Pinionszek_API/Models/DatabaseModel/PaymentStatus.cs
+++ Pinionszek_API/Models/DatabaseModel/PaymentStatus.cs
@@ -1,8 +1,12 @@
-﻿namespace Pinionszek_API.Models.DatabaseModel
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pinionszek_API.Models.DatabaseModel
 {
     public class PaymentStatus
     {
         public int IdPaymentStatus { get; set; }
         public string Name { get; set; }
+
+        public virtual ICollection<Payment> Payment { get; set; }
     }
 }
