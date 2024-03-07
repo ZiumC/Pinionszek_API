838d3cc635831ff138614e94e0bffca90da78693
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-03-07 12:09:13 +0100

    Added db in memory, not seeded yet

---
+++ Pinionszek_API.Tests/DbContexts/InMemContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pinionszek_API.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinionszek_API.Tests.DbContexts
+{
+    public class InMemContext
+    {
+        private async Task<ProdDbContext> GetDatabaseContext() 
+        {
+            var options = new DbContextOptionsBuilder<ProdDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var prodDbContext = new ProdDbContext(options);
+            prodDbContext.Database.EnsureCreated();
+
+            return prodDbContext;
+        }
+    }
+}
