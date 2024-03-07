679b6d0aba6ca850209eb8836bbea3981799c731
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-03-07 12:01:56 +0100

    Adding DI to tests

---
+++ Pinionszek_API.Tests/Controller/BudgetController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using Pinionszek_API.Services.DatabaseServices.BudgetService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinionszek_API.Tests.Controller
+{
+    public class BudgetController
+    {
+        private readonly IBudgetApiService _budgetService;
+        private readonly IMapper _mapper;
+
+        public BudgetController()
+        {
+            _budgetService = A.Fake<IBudgetApiService>();
+            _mapper = A.Fake<IMapper>();
+        }
+    }
+}
