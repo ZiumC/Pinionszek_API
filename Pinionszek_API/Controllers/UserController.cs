0d61832b1a836d1006492425ab8e7fb7c2ff16b5
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-04-08 14:35:54 +0200

    Created pagination for endpoint: /User/{idUser}/friends

--- Pinionszek_API/Controllers/UserController.cs
+++ Pinionszek_API/Controllers/UserController.cs
@@ -31,9 +31,11 @@ namespace Pinionszek_API.Controllers
         /// Get user friends by user ID 
         /// </summary>
         /// <param name="idUser">User ID</param>
+        /// <param name="page">Page number</param>
+        /// <param name="pageSize">Page size</param>
         [HttpGet("{idUser}/friends")]
         [ProducesResponseType(200, Type = typeof(IEnumerable<GetUserFriendDto>))]
-        public async Task<IActionResult> GetUserFriendsAsync(int idUser)
+        public async Task<IActionResult> GetUserFriendsAsync(int idUser, int page = 1, int pageSize = 20)
         {
             if (idUser <= 0)
             {
@@ -41,7 +43,23 @@ namespace Pinionszek_API.Controllers
                 return BadRequest(ModelState);
             }
 
+            if (page <= 0)
+            {
+                ModelState.AddModelError("error", "Page number is invalid");
+                return BadRequest(ModelState);
+            }
+
+            if (pageSize <= 0)
+            {
+                ModelState.AddModelError("error", "Page size is invalid");
+                return BadRequest(ModelState);
+            }
+
             var userFriendsData = await _userService.GetUserFriendsAsync(idUser);
+            userFriendsData = userFriendsData?
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
             if (userFriendsData == null || userFriendsData.Count() == 0)
             {
                 return NotFound();
