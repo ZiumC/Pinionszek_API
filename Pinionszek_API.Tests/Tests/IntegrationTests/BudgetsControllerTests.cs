2ab3abae73f0411b68342e4cb2806ead99dc3378
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-04-10 16:03:35 +0200

    Refactored few test in BudgetController

--- Pinionszek_API.Tests/Tests/IntegrationTests/BudgetsControllerTests.cs
+++ Pinionszek_API.Tests/Tests/IntegrationTests/BudgetsControllerTests.cs
@@ -32,6 +32,7 @@ namespace Pinionszek_API.Tests.Tests.IntegrationTests
         private readonly int _user_3 = 3;
         private readonly int _user_4 = 4;
         private readonly int _user_5 = 100;
+        private readonly int _budget_year = 2024;
         private readonly DateTime _budgetDate;
         private readonly IMapper _mapper;
         private readonly IConfiguration _config;
@@ -139,73 +140,64 @@ namespace Pinionszek_API.Tests.Tests.IntegrationTests
             var budgetApiService = new BudgetApiService(await dbContext);
             var paymentApiService = new PaymentApiService(await dbContext);
             var budgetController = new BudgetsController(_config, budgetApiService, paymentApiService, _mapper);
-            int budget_year = 2024;
+            var budgetYears = new { invalid1 = -1, invalid2 = 99999 };
 
             //Act
-            var okRequest_1 = await budgetController.GetBudgetsAsync(budget_year, 1);
-            var okActionResult_1 = okRequest_1 as OkObjectResult;
-            var budgetsResult_1 = okActionResult_1?.Value as IEnumerable<GetBudgetSummaryDto>;
+            var okRequest_1 = await budgetController.GetBudgetsAsync(_budget_year, _user_1);
+            var budgetsResult_1 = (okRequest_1 as OkObjectResult)?.Value as IEnumerable<GetBudgetSummaryDto>;
 
-            var okRequest_2 = await budgetController.GetBudgetsAsync(budget_year, 2);
-            var okActionResult_2 = okRequest_2 as OkObjectResult;
-            var budgetsResult_2 = okActionResult_2?.Value as IEnumerable<GetBudgetSummaryDto>;
+            var okRequest_2 = await budgetController.GetBudgetsAsync(_budget_year, _user_2);
+            var budgetsResult_2 = (okRequest_2 as OkObjectResult)?.Value as IEnumerable<GetBudgetSummaryDto>;
 
-            var okRequest_3 = await budgetController.GetBudgetsAsync(budget_year, 3);
-            var okActionResult_3 = okRequest_3 as OkObjectResult;
-            var budgetsResult_3 = okActionResult_3?.Value as IEnumerable<GetBudgetSummaryDto>;
+            var okRequest_3 = await budgetController.GetBudgetsAsync(_budget_year, _user_3);
+            var budgetsResult_3 = (okRequest_3 as OkObjectResult)?.Value as IEnumerable<GetBudgetSummaryDto>;
 
-            var notFoundRequest_1 = await budgetController.GetBudgetsAsync(budget_year, 4);
+            var notFoundRequest_1 = await budgetController.GetBudgetsAsync(_budget_year, _user_4);
 
-            var badRequest_1 = await budgetController.GetBudgetsAsync(budget_year, -1005);
-            var badRequestActionResult_1 = badRequest_1 as BadRequestObjectResult;
-            var badRequestResult_1 = badRequestActionResult_1?.Value as string;
+            var badRequest_1 = await budgetController.GetBudgetsAsync(_budget_year, -_user_5);
+            var badRequestResult_1 = (badRequest_1 as BadRequestObjectResult)?.Value as string;
 
-            var badRequest_2 = await budgetController.GetBudgetsAsync(-1, 1);
-            var badRequestActionResult_2 = badRequest_2 as BadRequestObjectResult;
-            var badRequestResult_2 = badRequestActionResult_2?.Value as string;
+            var badRequest_2 = await budgetController.GetBudgetsAsync(budgetYears.invalid1, _user_1);
+            var badRequestResult_2 = (badRequest_2 as BadRequestObjectResult)?.Value as string;
 
-            var badRequest_3 = await budgetController.GetBudgetsAsync(99999, 1);
-            var badRequestActionResult_3 = badRequest_3 as BadRequestObjectResult;
-            var badRequestResult_3 = badRequestActionResult_3?.Value as string;
+            var badRequest_3 = await budgetController.GetBudgetsAsync(budgetYears.invalid2, _user_1);
+            var badRequestResult_3 = (badRequest_3 as BadRequestObjectResult)?.Value as string;
 
             //Assert
             okRequest_1.Should().BeOfType<OkObjectResult>();
-            okActionResult_1.Should().NotBeNull();
             budgetsResult_1.Should().NotBeNull();
             budgetsResult_1?.Count().Should().Be(12);
             budgetsResult_1?
                 .Where(br => string.IsNullOrEmpty(br.Budget.Status))
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_1?
-                .Where(br => br.Budget.BudgetYear.Year != budget_year)
+                .Where(br => br.Budget.BudgetYear.Year != _budget_year)
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_1?
                 .Where(br => br.Needs < 0 || br.Wants < 0 || br.Savings < 0)
                 .ToList().Should().BeNullOrEmpty();
 
             okRequest_2.Should().BeOfType<OkObjectResult>();
-            okActionResult_2.Should().NotBeNull();
             budgetsResult_2.Should().NotBeNull();
             budgetsResult_2?.Count().Should().Be(12);
             budgetsResult_2?
                 .Where(br => string.IsNullOrEmpty(br.Budget.Status))
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_2?
-                .Where(br => br.Budget.BudgetYear.Year != budget_year)
+                .Where(br => br.Budget.BudgetYear.Year != _budget_year)
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_2?
                 .Where(br => br.Needs < 0 || br.Wants < 0 || br.Savings < 0)
                 .ToList().Should().BeNullOrEmpty();
 
             okRequest_3.Should().BeOfType<OkObjectResult>();
-            okActionResult_3.Should().NotBeNull();
             budgetsResult_3.Should().NotBeNull();
             budgetsResult_3?.Count().Should().Be(12);
             budgetsResult_3?
                 .Where(br => string.IsNullOrEmpty(br.Budget.Status))
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_3?
-                .Where(br => br.Budget.BudgetYear.Year != budget_year)
+                .Where(br => br.Budget.BudgetYear.Year != _budget_year)
                 .ToList().Should().BeNullOrEmpty();
             budgetsResult_3?
                 .Where(br => br.Needs < 0 || br.Wants < 0 || br.Savings < 0)
@@ -214,15 +206,12 @@ namespace Pinionszek_API.Tests.Tests.IntegrationTests
             notFoundRequest_1.Should().BeOfType<NotFoundResult>();
 
             badRequest_1.Should().BeOfType<BadRequestObjectResult>();
-            badRequestActionResult_1?.Value.Should().NotBeNull();
             badRequestResult_1?.Contains("is invalid").Should().BeTrue();
 
             badRequest_2.Should().BeOfType<BadRequestObjectResult>();
-            badRequestActionResult_2?.Value.Should().NotBeNull();
             badRequestResult_2?.Contains("is invalid").Should().BeTrue();
 
             badRequest_3.Should().BeOfType<BadRequestObjectResult>();
-            badRequestActionResult_3?.Value.Should().NotBeNull();
             badRequestResult_3?.Contains("is invalid").Should().BeTrue();
         }
 
